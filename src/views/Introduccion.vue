1082df752ca73deb2834e94dd9c346261c8ab518
Francisco Vásquez <francisjvasquez23@gmail.com>
2025-12-03 20:43:00 -0500

    Avance
    Avance

--- src/views/Introduccion.vue
+++ src/views/Introduccion.vue
@@ -85,7 +85,7 @@
       .col-lg-12
         figure
           .video
-            iframe(width="560" height="315" src="https://www.youtube.com/embed/vdPrCjWJSHo?si=X4NxENRo3LLXAua_" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen)
+            iframe(width="560" height="315" src="https://www.youtube.com/embed/xR2CSMh9Y_c" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen)
 
 </template>
 
